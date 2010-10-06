25e341ac4f3020a31128790fdc8de93f11e3d99b
Jonathan Ballet <jon@multani.info>
2010-10-06 19:21:43 +0200

    Colorize the 'Match All' box when the regex matches the input

--- kodos/main.py
+++ kodos/main.py
@@ -2,6 +2,7 @@ import re
 import sys
 
 from PyQt4.QtGui import QApplication, QMainWindow
+from PyQt4.QtGui import QTextCursor, QTextCharFormat, QColor
 
 from kodos import widgets
 from kodos.ui.ui_main import Ui_MainWindow
@@ -48,10 +49,31 @@ class KodosMainWindow(QMainWindow, Ui_MainWindow):
             return
 
         # The regex match the input!
-        self.statusbar.showMessage("Pattern matches (found %d match)" %
-                                   len(r.findall(search)))
-        self.statusbar.setIndicator('ok')
+        self.matchNumberBox.setMinimum(1)
+        self.matchText.setPlainText(search)
+        self.matchAllText.setPlainText(search)
 
+
+        # Compute results in the various result panels
+        for i, match in enumerate(r.finditer(search)):
+            # Update the select match number widget
+            self.matchNumberBox.setMaximum(i + 1)
+
+            # Update the matchAll text widget
+            format = QTextCharFormat()
+            format.setForeground(QColor('blue'))
+
+            cursor = QTextCursor(self.matchAllText.document())
+            cursor.setPosition(match.start())
+            cursor.movePosition(
+                QTextCursor.NextCharacter,
+                QTextCursor.KeepAnchor,
+                match.end() - match.start())
+            cursor.setCharFormat(format)
+
+
+        self.statusbar.setIndicator('ok')
+        self.statusbar.showMessage("Pattern matches (found %d match)" % (i + 1))
 
 
 def run(args=None):
